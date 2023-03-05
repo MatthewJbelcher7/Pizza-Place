9b44eafb2a757da7c1f8bbaa495632ef567bf1ed
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-03-05 06:56:47 -0700

    fixed syntax errors and created a display that refreshes on each use

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ Pizza.prototype.calcCost = function() {
       this.toppings.meat.forEach(function(meat) {
         cost += meat.price;
       });
-      this.toppings.veggies.forEach(function(veg) {
+      this.toppings.veggie.forEach(function(veg) {
         cost += veg.price;
       });
       this.toppings.cheese.forEach(function(cheese) {
@@ -56,7 +56,7 @@ let pizzaToppings = {
     { name: "sausage", price:1},
     { name: "chicken", price:1.5},
 ],
-  veggies: [
+  veggie: [
     { name: "mushroom", price:0},
     { name: "jalapeno", price:0},
     { name:  "olive", price:0},
@@ -72,15 +72,30 @@ let pizzaToppings = {
 const pizzaForm = document.getElementById("pizza-form");
 pizzaForm.addEventListener("submit", function(event) {
   event.preventDefault();
+  const sauces = [];
+  const meats = [];
+  const veggies = [];
+  const cheeses = [];
   const name = document.getElementById("guest-name").value;
-  const size = document.querySelector('input[name="size"];checked').value;
-  const sauce = document.querySelector('input[name="sauce"];checked').value;
-  const meat = document.querySelector('input[name="meat"];checked').value;
-  const veggies = document.querySelector('input[name="veggies"];checked').value;
-  const cheese = document.querySelector('input[name="cheese"];checked').value;
+  const size = document.querySelector('input[name="size"]:checked').value;
+  document.querySelectorAll('input[name="sauce"]:checked').forEach(function(checkbox) {
+    sauces.push(checkbox.value);
+  });
+  document.querySelectorAll('input[name="meat"]:checked').forEach(function(checkbox) {
+    meats.push(checkbox.value);
+  });
+  document.querySelectorAll('input[name="veggies"]:checked').forEach(function(checkbox) {
+    veggies.push(checkbox.value);
+  });
+  document.querySelectorAll('input[name="cheese"]:checked').forEach(function(checkbox) {
+    cheeses.push(checkbox.value);
+  });
 
   const pizza = new Pizza(name, pizzaToppings, size);
   const cost = pizza.calcCost();
 
   console.log(`Order for ${name}! with a ${size} pizza for $${cost}`);
+  const orderUp = document.getElementById("order-up");
+  orderUp.innerHTML = `Order for ${name}! With a ${size} pizza for $${cost}
+  `;
 });  
